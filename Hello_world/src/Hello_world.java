7c3847e9a60213d00ab0a0b710016cb295bb6c25
leesukjun <leesukjun@users.noreply.github.com>
2017-08-21 09:13:51 -0700

    Hello_world Work1

---
+++ Hello_world/src/Hello_world.java
@@ -0,0 +1,13 @@
+/**
+ * This is my first program
+ * @author APCS2
+ *8.21.2017
+ */
+public class Hello_world {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
