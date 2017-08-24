dc2387668fcd13a124d7c969bf76948291a8cde5
leesukjun <leesukjun@users.noreply.github.com>
2017-08-24 09:30:30 -0700

    First Work for Project 2

---
+++ Project_2/src/Project_2.java
@@ -0,0 +1,14 @@
+//James Lee
+/*August 23, 2017
+Project 2 -- Use print and println*/
+public class Project_2 {
+
+	public static void main(String[] args) {
+		System.out.print("hi there1");
+		System.out.println("hi there2");
+		System.out.print("hi there3");
+		// TODO Auto-generated method stub
+
+	}
+
+}
