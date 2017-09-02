fc7d12d87cbcb7d91d780fed64c7a10d0e9d18da
leesukjun <31090646+leesukjun@users.noreply.github.com>
2017-09-01 20:44:20 -0700

    two methods for our GPA class (calculateGPA & printGPA)

--- My_gpa/src/My_gpa.java
+++ My_gpa/src/My_gpa.java
@@ -6,13 +6,13 @@ public class My_gpa {
 		double grade1 = 4.0;
 		double grade2 = 3.0;
 		double grade3 = 2.0;
-		double gpa = averageGPA(grade1, grade2, grade3);
+		double gpa = calculateGPA(grade1, grade2, grade3);
 		printGPA(gpa);
 	}
 	public static void printGPA(double gpa) {
 			System.out.println(gpa);
 	}
-	public static double averageGPA(double grade1, double grade2, double grade3) {
+	public static double calculateGPA(double grade1, double grade2, double grade3) {
 			double sum = grade1 + grade2 + grade3;
 			double GPA = sum/3;	
 			return GPA;
