b36fc4216e3c8abbe0aa057734a0f8adaa23cb05
leesukjun <31090646+leesukjun@users.noreply.github.com>
2017-09-01 20:41:27 -0700

    Averaging my GPA by a new method

---
+++ My_gpa/src/My_gpa.java
@@ -0,0 +1,20 @@
+//James Lee
+//September 1, 2017
+//Making new methods
+public class My_gpa {
+	public static void main(String [] args) {
+		double grade1 = 4.0;
+		double grade2 = 3.0;
+		double grade3 = 2.0;
+		double gpa = averageGPA(grade1, grade2, grade3);
+		printGPA(gpa);
+	}
+	public static void printGPA(double gpa) {
+			System.out.println(gpa);
+	}
+	public static double averageGPA(double grade1, double grade2, double grade3) {
+			double sum = grade1 + grade2 + grade3;
+			double GPA = sum/3;	
+			return GPA;
+	}
+}
