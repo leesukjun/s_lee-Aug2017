5b971bb98d504f31be7b93be4e6dd67253a56e3d
leesukjun <31090646+leesukjun@users.noreply.github.com>
2017-09-29 23:39:29 -0700

    Finished

--- Access Calculate/src/DoMath.java
+++ Access Calculate/src/DoMath.java
@@ -5,6 +5,7 @@
  */
 public class DoMath {
 	public static void main(String[] args) {
-		System.out.println(Calculate.round2(-4.237823472973447));
+		System.out.println(Calculate.sqrt(2.25));
+		System.out.println(Calculate.quadForm(1, -4, 4));
 	}
 }
