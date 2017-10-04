b34b59f5e48217eafc784961856b85de707a38cf
leesukjun <leesukjun@users.noreply.github.com>
2017-10-04 10:48:55 -0700

    Finished

---
+++ Partner Project/src/ProcessingNumbers.java
@@ -0,0 +1,51 @@
+import java.util.*;
+public class ProcessingNumbers {
+	public static void main(String args []) {
+		Scanner userInput = new Scanner(System.in);
+		System.out.println("How many terms are you going to enter?");
+		int terms = userInput.nextInt();
+		System.out.println("Enter your " + terms + " integers");
+		int max = userInput.nextInt();
+		int min = max;
+		int inputNumber;
+		int evenSum = 0;
+		int maxEven = 69;//any odd number that correspond to if statement in line 43
+		boolean firstEven = true;
+		if(min % 2 == 0) {
+			evenSum = min;
+			maxEven = min;
+			firstEven = false;
+		}
+		for(int i = 1; i < terms; i++) {
+			inputNumber = userInput.nextInt();
+			if (inputNumber % 2 == 0) {
+				if (firstEven == true) {
+					maxEven = inputNumber;
+					evenSum = inputNumber;
+					firstEven = false;
+				}
+				else {
+					if (inputNumber > maxEven) {
+						maxEven = inputNumber;
+					}
+				evenSum += inputNumber;
+				}
+			}
+			if (inputNumber > max) {
+				max = inputNumber;
+			}
+			if (inputNumber < min) {
+				min = inputNumber;
+			}
+		}		
+		
+	System.out.println("min: " + min + " max: " + max);
+	if (maxEven != 69 ) {
+		System.out.println("Sum of even numbers: " + evenSum + " Largest even number: " + maxEven);
+	} else {
+		System.out.println("Sum of even numbers: " + evenSum + " Largest even number: Does not exist"
+				);
+	}
+	userInput.close();	
+	}
+}
