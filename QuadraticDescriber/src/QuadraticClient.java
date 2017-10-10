a13d65dd3da3348b806b695cf65e5ab34c2c9157
leesukjun <leesukjun@users.noreply.github.com>
2017-10-10 11:05:41 -0700

    Finnished

--- QuadraticDescriber/src/QuadraticClient.java
+++ QuadraticDescriber/src/QuadraticClient.java
@@ -7,13 +7,17 @@ public class QuadraticClient {
 		Scanner userInput = new Scanner(System.in);
 		boolean done = false;
 		while (!done) {
+			System.out.println("type the coefficient of x^2");
 			int a = userInput.nextInt();
+			System.out.println("type the coefficient of x");
 			int b = userInput.nextInt();
+			System.out.println("type the value of constant");
 			int c = userInput.nextInt();
 			System.out.println(Quadratic.quadrDescriber(a, b, c));
-			System.out.println("Do you want to keep going? (Type \"1\" to end)");
-			int answer = userInput.nextInt();
-			if (answer==1) {
+			System.out.println("Do you want to keep going? (Type \"quit\" to end)");
+			String answer = userInput.next();
+			String check = "quit";
+			if (answer.toLowerCase().equals(check)) {
 				done = true;
 			}
 		}
