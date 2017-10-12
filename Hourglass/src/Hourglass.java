8ad2c7a8b21a732093023607fc4b8aea379ec4a5
leesukjun <leesukjun@users.noreply.github.com>
2017-10-12 09:18:10 -0700

    Finished the hour glass

---
+++ Hourglass/src/Hourglass.java
@@ -0,0 +1,48 @@
+
+public class Hourglass {
+	public static void main(String[] args) {
+		cap();
+		topHalf();
+		middle();
+		bottomHalf();
+		cap();
+	}
+	public static void cap(){
+		System.out.print("|");
+		for (int i = 1; i<=10;i++){
+			System.out.print("\"");
+		}
+		System.out.println("|");
+	}
+	public static void topHalf(){
+		int colonDecrement = 0;
+		for (int i=1; i <=4; i++){
+			for (int j=1; j<=i; j++){
+				System.out.print(" ");
+			}
+			System.out.print("\\");
+			for (int j=4; j> colonDecrement; j--) { 
+				System.out.print("::");
+			}
+			System.out.println("/");
+			colonDecrement++;
+			}
+		}
+	public static void middle() {
+		System.out.println("     ||     ");
+	}
+	public static void bottomHalf(){
+		int colonIncrement = 1;
+		for (int i=4; i >=1; i--){
+			for (int j=1; j<=i; j++){
+				System.out.print(" ");
+			}
+			System.out.print("/");
+			for (int j=1; j<=colonIncrement; j++) { 
+				System.out.print("::");
+			}
+			System.out.println("\\");
+			colonIncrement++;
+		}
+	}
+}
