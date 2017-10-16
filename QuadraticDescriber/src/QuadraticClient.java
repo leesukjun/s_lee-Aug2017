d44c05ec8952dfe34d3d11101f078113c2497b18
James <James@DESKTOP-VSMC0R8>
2017-10-16 08:59:38 -0700

    finished

--- QuadraticDescriber/src/QuadraticClient.java
+++ QuadraticDescriber/src/QuadraticClient.java
@@ -9,17 +9,17 @@ public class QuadraticClient {
 		Scanner userInput = new Scanner(System.in);
 		boolean done = false;
 		while (!done) {
-			System.out.print("\na: ");
+			System.out.print("\na: ");//takes the coefficient of x^2
 			double a = userInput.nextDouble();
-			System.out.print("b: ");
+			System.out.print("b: ");//takes the coefficient of x
 			double b = userInput.nextDouble();
-			System.out.print("c: ");
+			System.out.print("c: ");//takes the constant
 			double c = userInput.nextDouble();
 			System.out.println("\n" + Quadratic.quadrDescriber(a, b, c));
 			System.out.println("\nDo you want to keep going? (Type \"quit\" to end)");
 			String answer = userInput.next();
 			String check = "quit";
-			if (answer.toLowerCase().contains(check)) {
+			if (answer.toLowerCase().contains(check)) {//if the input contains the word "quit" regardless of the caps
 				done = true;
 			}
 		}
