767c36b83c268785dbb05f0662f9000cc3b9d18d
leesukjun <31090646+leesukjun@users.noreply.github.com>
2017-10-27 12:48:00 -0700

    Update Split.java
    Finished

--- Split/src/Split.java
+++ Split/src/Split.java
@@ -1,11 +1,24 @@
 /* James Lee
  * 10.26.2017
- * Split!!!
+ * SplitString 
  */
+
+ //Your task Part 1:
+/*Write a method that take in a string like "applespineapplesbreadlettustomatobaconmayohambreadcheese" describing a sandwich
+* use String.split to split up the sandwich by the word "bread" and return what's in the middle of the sandwich and ignores what's on the outside
+* What if it's a fancy sandwich with multiple pieces of bread?
+*/
+
+//Your task Part 2:
+/*Write a method that take in a string like "apples pineapples bread lettus tomato bacon mayo ham bread cheese" describing a sandwich
+* use String.split to split up the sandwich at the spaces, " ", and return what's in the middle of the sandwich and ignores what's on the outside
+* Again, what if it's a fancy sandwich with multiple pieces of bread?
+*/
+
 import java.util.Arrays;
 
 public class Split {
-	public static void main(String[] args) {
+	public static void main(String[] args) {//practice
 		String[] arr = "I like apples!".split(" ");
 		System.out.println(Arrays.toString(arr));
 		
@@ -15,8 +28,10 @@ public class Split {
 		System.out.println(Arrays.toString("really like really red apples".split("really")));
 		System.out.println(Arrays.toString("I really like really red apples,really".split("really")));
 		
+		System.out.println(sandwichSplit_V1("applespineapplesbreadlettustomatobaconmayohambreadcheese"));
+		System.out.println(sandwichSplit_V2("apples pineapples bread lettus tomato bacon mayo ham bread cheese"));
 	}
-	public static void sandwichSplit(String x) {//combine the if statements (use &&)
+	public static void sandwichSplit_V1(String x) {//combine the if statements (use &&)
 		if (x.indexOf("bread") >= 0) {
 			if (x.lastIndexOf("bread") != x.indexOf("bread") + 5) {
 				if (x.lastIndexOf("bread") != x.indexOf("bread")) {
@@ -42,7 +57,7 @@ public class Split {
 		System.out.println("This is not a sandwich");
 		}
 	}
-	public static void sandwichSplitBySpaces(String x) {//combine the if statements (use &&)
+	public static void sandwichSplit_V2(String x) {//combine the if statements (use &&)
 		if (x.indexOf("bread") >= 0) {
 			if (x.lastIndexOf("bread") != x.indexOf("bread") + 6) {
 				if (x.lastIndexOf("bread") != x.indexOf("bread")) {
