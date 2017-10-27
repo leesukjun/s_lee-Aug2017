4089f67d092c2e303874dd8f92ee0299bbaefced
leesukjun <31090646+leesukjun@users.noreply.github.com>
2017-10-27 12:42:03 -0700

    Update Split.java

--- Split/src/Split.java
+++ Split/src/Split.java
@@ -14,13 +14,58 @@ public class Split {
 		System.out.println(Arrays.toString("I really like reallyreally red apples".split("really")));
 		System.out.println(Arrays.toString("really like really red apples".split("really")));
 		System.out.println(Arrays.toString("I really like really red apples,really".split("really")));
+		
 	}
-	public static String split(String line, String check) {
-		for (int i=1; i<=(line.length()-check.length()); i++) {
-			if (line.substring(i, check.length())==check) {
-				
+	public static void sandwichSplit(String x) {//combine the if statements (use &&)
+		if (x.indexOf("bread") >= 0) {
+			if (x.lastIndexOf("bread") != x.indexOf("bread") + 5) {
+				if (x.lastIndexOf("bread") != x.indexOf("bread")) {
+					String y = x.substring(x.indexOf("bread") + 5, x.lastIndexOf("bread"));
+					System.out.println(y);
+					String[] s = y.split("bread");
+					if (s.length > 0) {
+					   System.out.println(Arrays.toString(s));
+					}
+					else {
+					   System.out.println("This is not a sandwich");
+					}
+				}
+				else {
+					System.out.println("This is not a sandwich");
+				}
+			}
+			else {
+				System.out.println("This is not a sandwich");
+			}
+			}
+		else {
+		System.out.println("This is not a sandwich");
+		}
+	}
+	public static void sandwichSplitBySpaces(String x) {//combine the if statements (use &&)
+		if (x.indexOf("bread") >= 0) {
+			if (x.lastIndexOf("bread") != x.indexOf("bread") + 6) {
+				if (x.lastIndexOf("bread") != x.indexOf("bread")) {
+					String y = x.substring(x.indexOf("bread") + 6, x.lastIndexOf("bread"));
+					String[] s = y.split(" ");
+					System.out.println(y);
+					if (s.length > 0) {
+					   System.out.println(Arrays.toString(s));
+					}
+					else {
+					   System.out.println("This is not a sandwich");
+					}
+				}
+				else {
+					System.out.println("This is not a sandwich");
+				}
+			}
+			else {
+				System.out.println("This is not a sandwich");
 			}
 		}
-		return line;
+		else {
+		System.out.println("This is not a sandwich");
+		}
 	}
 }
