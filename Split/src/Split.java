d03467a8159c8ab33b7b901d65882c80a7ed7f14
leesukjun <leesukjun@users.noreply.github.com>
2017-10-30 09:40:53 -0700

    Doned

--- Split/src/Split.java
+++ Split/src/Split.java
@@ -26,42 +26,30 @@ public class Split {
 		System.out.println(Arrays.toString("really like really red apples".split("really")));
 		System.out.println(Arrays.toString("I really like really red apples,really".split("really")));
 		
-		sandwichSplit_V1("applespineapplesbreadlettustomatobaconmayohambreadcheese");
-		sandwichSplit_V2("apples pineapples bread lettus tomato bacon mayo ham bread cheese");
+		sandwichSplit_V1("applespineapplesbreadlettustomatobaconmayohambreadcheesebread");
+		sandwichSplit_V2("bread apples pineapples bread lettus tomato bacon mayo ham bread cheese bread");
 	}
-	public static void sandwichSplit_V1(String x) {
+	public static void sandwichSplit_V1(String x) {//part 1
 		if (x.indexOf("bread") >= 0 && x.lastIndexOf("bread") != x.indexOf("bread") + 5) {
 			if (x.lastIndexOf("bread") != x.indexOf("bread")) {
 				String y = x.substring(x.indexOf("bread") + 5, x.lastIndexOf("bread"));
+				y = y.replaceAll("bread","");
 				System.out.println(y);
-				String[] s = y.split("bread");
-				if (s.length > 0) {
-				   System.out.println(Arrays.toString(s));
-				}
-				else {
-				   System.out.println("This is not a sandwich");
-				}
 			}
 			else {
 				System.out.println("This is not a sandwich");
 			}
-		}
-		else {
+		} else {
 			System.out.println("This is not a sandwich");
 		}
 	}
-	public static void sandwichSplit_V2(String x) {//combine the if statements (use &&)
+	public static void sandwichSplit_V2(String x) {//part 2
 		if (x.indexOf("bread") >= 0 && x.lastIndexOf("bread") != x.indexOf("bread") + 6) {
 			if (x.lastIndexOf("bread") != x.indexOf("bread")) {
 				String y = x.substring(x.indexOf("bread") + 6, x.lastIndexOf("bread"));
-				String[] s = y.split(" ");
+				y = y.replaceAll("bread","");
+				y = y.replaceAll("\\s+","");
 				System.out.println(y);
-				if (s.length > 0) {
-				   System.out.println(Arrays.toString(s));
-				}
-				else {
-				   System.out.println("This is not a sandwich");
-				}
 			}
 			else {
 				System.out.println("This is not a sandwich");
