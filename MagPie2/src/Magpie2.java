5829ecff05bcdb3be76077e06cd01460108d0e98
leesukjun <leesukjun@users.noreply.github.com>
2017-11-08 09:51:31 -0800

    Finished part 2

--- MagPie2/src/Magpie2.java
+++ MagPie2/src/Magpie2.java
@@ -13,13 +13,35 @@ public class Magpie2 {
 	 */
 	public String getResponse(String statement) {
 		String response = "";
-		if (statement.indexOf("no") >= 0) {
+		if (statement.trim().length() < 1) {
+				response = "Say something, please.";
+		}
+		else if (statement.indexOf("no") >= 0) {
 			response = "Why so negative?";
 		} else if (statement.indexOf("mother") >= 0
 				|| statement.indexOf("father") >= 0
 				|| statement.indexOf("sister") >= 0
 				|| statement.indexOf("brother") >= 0) {
 			response = "Tell me more about your family.";
+		} else if (statement.indexOf("Dreyer") >= 0
+				|| statement.indexOf("deHeer") >= 0
+				|| statement.indexOf("Lamont") >= 0) {
+			response = "I heard your teachers are very good at teaching!";
+		} else if (statement.indexOf("Trump") >= 0
+				|| statement.indexOf("Donald") >= 0
+				|| statement.indexOf("trump") >= 0
+				|| statement.indexOf("donald") >= 0) {
+			response = "Let's make America great again!";
+		} else if (statement.indexOf("Hillary") >= 0
+				|| statement.indexOf("Clinton") >= 0
+				|| statement.indexOf("hillary") >= 0
+				|| statement.indexOf("clinton") >= 0) {
+			response = "Make sure you don't mess around with your email!";
+		} else if (statement.indexOf("Test") >= 0
+				|| statement.indexOf("test") >= 0
+				|| statement.indexOf("Quiz") >= 0
+				|| statement.indexOf("quiz") >= 0) {
+			response = "Haha, sucks for you!";
 		} else {
 			response = getRandomResponse();
 		}
@@ -31,7 +53,7 @@ public class Magpie2 {
 	 * returns a non-committal string
 	 */
 	private String getRandomResponse() {
-		final int NUMBER_OF_RESPONSES = 4;
+		final int NUMBER_OF_RESPONSES = 6;
 		double r = Math.random();
 		int whichResponse = (int) (r * NUMBER_OF_RESPONSES);
 		String response = "";
@@ -44,8 +66,11 @@ public class Magpie2 {
 			response = "Do you really think so?";
 		} else if (whichResponse == 3) {
 			response = "You don't say.";
+		} else if (whichResponse == 4) {
+			response = "That is very interesting.";
+		} else if (whichResponse == 5) {
+			response = "Wow.";
 		}
-
 		return response;
 	}
 }
